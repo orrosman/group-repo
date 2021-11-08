8c0113ed816910c8eff32e8280609fcecebf25f6
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-11-08 09:53:41 +0200

    task 3.2 - return a person by id

--- data/phonebook.js
+++ data/phonebook.js
@@ -11,4 +11,15 @@ function getPhonebookLength() {
 	return phonebook.data.length;
 }
 
-module.exports = { getPhonebook, getPhonebookLength };
+function getPerson(id) {
+	const data = fs.readFileSync('./data/phonebook.json');
+	const phonebook = JSON.parse(data);
+	for (const person of phonebook.data) {
+		if (person.id == id) {
+			return person;
+		}
+	}
+	return false;
+}
+
+module.exports = { getPhonebook, getPhonebookLength, getPerson };
--- routers/personsRouter.js
+++ routers/personsRouter.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const Phonebook = require('../data/phonebook');
 
+router.get('/:id', (req, res) => {
+	const id = req.params.id;
+	const person = Phonebook.getPerson(id);
+
+	res.send(person);
+});
+
 router.get('/', (req, res) => {
 	const phonebook = Phonebook.getPhonebook();
 
