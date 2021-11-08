9430e6d432ac928f97fa52a7d457150c4d309262
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-11-08 14:04:16 +0200

    task 3.8 - custom morgan output

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const morgan = require('morgan');
 app.use(cors());
 app.use(express.json());
 
-app.use(morgan('tiny'));
+morgan.token('body', (req) => {
+	return JSON.stringify(req.body);
+});
+app.use(
+	morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
 
 app.use('/api/persons', personsRouter);
 app.use('/info', async (req, res) => {
